3fd5cf8263829c3f1ddb481743bb7fbfb002227d
markherras2003 <larrymark2003@gmail.com>
2023-04-27 12:16:01 +0800

    Update JobOrderService.js

--- client/src/service/JobOrderService.js
+++ client/src/service/JobOrderService.js
@@ -5,24 +5,4 @@ export default class JobOrderService {
         const response = await axios.get('/joborder');
         return response.data.data;
     }
-
-    async handleAdd() {
-        try {
-            const response = await axios.post(`/joborder`, {
-                client_name: '',
-                unit_description: '',
-                unit_model: '',
-                unit_accessories: '',
-                unit_problem: '',
-                resolution: '',
-                received_by: '',
-                job_order_by: '',
-                tech_incharge: '',
-            });
-            console.log(response);
-        } catch (error) {
-                errorMessage.value = "An unknown error occurred.";
-        }
-    }
-
 }
